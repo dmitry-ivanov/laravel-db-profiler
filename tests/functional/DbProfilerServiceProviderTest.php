204e4eb012b53f52dbd957e7c647f180d9546b64
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-01 22:19:58 +0200

    DBP: Initial usage of orchestra.

--- tests/functional/DbProfilerServiceProviderTest.php
+++ tests/functional/DbProfilerServiceProviderTest.php
@@ -8,4 +8,11 @@ class DbProfilerServiceProviderTest extends TestCase
         $this->visit('/');
         $this->see('Hello World!');
     }
+
+    /** @test */
+    public function it_successfully_loaded_routes_from_fixture_second_test()
+    {
+        $this->visit('/');
+        $this->see('Hello World!');
+    }
 }
