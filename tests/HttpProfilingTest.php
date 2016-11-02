2ea30df96b5156e3a2a67fcf7ad0f57464b84807
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-02 18:34:10 +0200

    DBP: Local env tests added.

--- tests/HttpProfilingTest.php
+++ tests/HttpProfilingTest.php
@@ -25,10 +25,11 @@ class HttpProfilingTest extends TestCase
     /** @test */
     public function it_is_disabled_for_local_environment_if_no_vvv_request_param_set()
     {
-        // $this->assertTrue($this->app->isLocal());
-        // $this->visit('/');
-        //
-        // $this->see('Home page!');
-        // $this->dontSee('select * from posts');
+        $this->setApplicationEnvironment('local');
+        $this->assertTrue($this->app->isLocal());
+        $this->visit('/');
+
+        $this->see('Home page!');
+        $this->dontSee('select * from posts');
     }
 }
