2de8bc3ac3b1c6e3f2d7d8c0b8f6bcabd3b36f09
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-03 19:17:45 +0200

    DBP: Orchestra hi one more time!

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,41 @@
 <?php
 
-abstract class TestCase extends \PHPUnit\Framework\TestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    protected function tearDown()
+    protected function getEnvironmentSetUp($app)
     {
-        Mockery::close();
+        $this->setUpDatabase();
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpFactories();
+        $this->loadMigrations();
+        $this->seedDatabase();
+    }
+
+    private function setUpDatabase()
+    {
+        config(['database.default' => 'testing']);
+    }
+
+    private function setUpFactories()
+    {
+        $this->withFactories(__DIR__ . '/fixture/database/factories');
+    }
+
+    private function loadMigrations()
+    {
+        $this->loadMigrationsFrom([
+            '--database' => 'testing',
+            '--realpath' => __DIR__ . '/fixture/database/migrations',
+        ]);
+    }
+
+    private function seedDatabase()
+    {
+        factory(Post::class, 10)->create();
     }
 }
