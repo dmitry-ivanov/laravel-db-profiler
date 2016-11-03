24f6a914209ee107c9051ec47b8e9d6390009035
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-03 18:05:19 +0200

    DBP: Orchestra tests removed.

--- orchestra/ConsoleProfilingTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ConsoleProfilingTest extends TestCase
-{
-    /** @test */
-    public function it_()
-    {
-    }
-}
--- orchestra/fixture/app/Post.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Database\Eloquent\Model;
-
-class Post extends Model
-{
-    protected $fillable = ['title'];
-}
