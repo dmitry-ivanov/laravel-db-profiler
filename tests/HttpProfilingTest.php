7382df8669b30e63cbc4e31d4488deee69ff067a
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-03 16:37:15 +0200

    DBP: Http tests initial.

--- tests/HttpProfilingTest.php
+++ tests/HttpProfilingTest.php
@@ -3,26 +3,6 @@
 class HttpProfilingTest extends TestCase
 {
     /** @test */
-    public function it_is_disabled_for_non_local_environments()
-    {
-        $this->assertFalse($this->app->isLocal());
-        $this->visit('/');
-
-        $this->see('Home page!');
-        $this->dontSee('select * from posts');
-    }
-
-    /** @test */
-    public function it_is_disabled_for_non_local_environments_even_if_vvv_request_param_is_set()
-    {
-        $this->assertFalse($this->app->isLocal());
-        $this->visit('/?vvv');
-
-        $this->see('Home page!');
-        $this->dontSee('select * from posts');
-    }
-
-    /** @test */
     public function it_is_disabled_for_local_environment_if_no_vvv_request_param_was_set()
     {
         // $this->assertTrue($this->app->isLocal());
