f125655686ddc6e82c5ae410d58f4f62442057e5
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-03 22:16:38 +0200

    DBP: Console tests added.

---
+++ tests/ConsoleProfilingTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class ConsoleProfilingTest extends TestCase
+{
+    protected function runningInConsole()
+    {
+        return true;
+    }
+
+    protected function withVvv()
+    {
+        $_SERVER['argv']['-vvv'] = true;
+        return $this;
+    }
+
+    /** @test */
+    public function it_is_disabled_if_environment_is_not_local()
+    {
+        $this->notLocal()->boot();
+        $this->assertDbProfilerIsNotActivated();
+    }
+
+    /** @test */
+    public function it_is_disabled_if_environment_is_local_but_there_is_no_vvv_argument()
+    {
+        $this->local()->boot();
+        $this->assertDbProfilerIsNotActivated();
+    }
+
+    /** @test */
+    public function it_is_enabled_if_environment_is_local_and_there_is_vvv_argument()
+    {
+        $this->local()->withVvv()->boot();
+        $this->assertDbProfilerIsActivated();
+    }
+
+    /** @test */
+    public function it_dumps_all_database_queries_with_applied_bindings()
+    {
+        $this->local()->withVvv()->boot();
+        $this->assertDatabaseQueriesAreDumped();
+    }
+}
